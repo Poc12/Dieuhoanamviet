2487490628fd287f171b3b2477a537082207c399
thanhad <thanhadhp2000@gmail.com>
2023-12-14 11:17:06 +0700

    banner2.png

--- app/Http/Controllers/FrontEnd/Home/views/home.blade.php
+++ app/Http/Controllers/FrontEnd/Home/views/home.blade.php
@@ -220,13 +220,7 @@
                     </div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="tp-banner-item tp-banner-item-sm tp-banner-height p-relative mb-30 z-index-1 fix">
-                            <div class="tp-banner-thumb include-bg transition-3" data-background="{{url('assets/images/banner2.png')}}"></div>
-                            <div class="tp-banner-content">
-                                <h3 class="tp-banner-title">
-                                    <a href="product-details.html">HyperX Cloud II <br> Wireless</a>
-                                </h3>
-                                <p>Sale 35% off</p>
-                            </div>
+                            <div class="tp-banner-thumb include-bg transition-3" data-background="{{url('assets/images/banner3.png')}}"></div>
                         </div>
                     </div>
                 </div>
