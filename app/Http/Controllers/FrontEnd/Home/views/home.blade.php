95714abd285ba871c587e029cfa5c59d39f9f7c3
Thành Nguyễn <thanhadhp2000@gmail.com>
2024-03-22 07:44:42 +0000

    Update file home.blade.php

--- app/Http/Controllers/FrontEnd/Home/views/home.blade.php
+++ app/Http/Controllers/FrontEnd/Home/views/home.blade.php
@@ -356,7 +356,7 @@
         </section>
         <!-- product arrival area end -->
         <!-- product banner area start -->
-        <div class="tp-product-banner-area pb-20">
+        <!-- <div class="tp-product-banner-area pb-20"> -->
             <div class="container">
                 <div class="tp-product-banner-slider fix">
                     <div class="tp-product-banner-slider-active swiper-container">
